033824c0e76bea4cc15f36da09c409ce4306cb48
alemaulou <alemau@uw.edu>
2019-02-05 00:07:02 -0800

    changes

--- phyllotaxis-fun/sketch.js
+++ phyllotaxis-fun/sketch.js
@@ -12,7 +12,6 @@ function setup() {
   cv = createCanvas(800, 800);
   centerCanvas();
   angleMode(DEGREES);
-  colorMode(HSB);
   background("black");
 }
 
@@ -32,10 +31,16 @@ function draw() {
   var x = r * cos(a) + width/2;
   var y = r * sin(a) + height/2;
 
+  var x2 = r * cos(a) + width/3;
+  var y2 = r * sin(a) + height/3;
+
+  var x3 = r * cos(a) + width/4;
+  var y3 = r * sin(a) + height/4;
+
   fill((a-r) % 255, 255, 255, 255);
   noStroke();
   ellipse(x,y, 3, 3);
-
-
+  ellipse(x2, y2, 3, 3);
+  ellipse(x3, y3, 3, 3);
   n++;
 }
